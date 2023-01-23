59e1645d9eb77e2a019fb1e9d709a5e5419af3e8
Pulkitwebd <pulkitgupta714@gmail.com>
2023-01-23 12:35:00 +0530

    add blogCard and import in index.jsx

---
+++ client/src/component/Blogs/BlogCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography"
+
+const BlogCard = () => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="https://www.shutterstock.com/image-photo/image-butterfly-agama-lizard-leiolepis-600w-659854906.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default BlogCard;
--- client/src/component/Blogs/index.js
+++ client/src/component/Blogs/index.js
@@ -1,146 +1,47 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
-import { Classes } from "./Blogs.module.css";
-//import { makeStyles } from '@mui/material';
-
-//const useStyles=makeStyles({
-//
-// });
+// import { Classes } from "./Blogs.module.css";
+import BlogCard from "./BlogCard";
 
 export default function MediaCard() {
   //const classes = useStyles();
   return (
-    <Grid container columnSpacing={{md:1}} rowSpacing={2} paddingLeft={7} marginTop={4} marginBottom={4}
+    <Grid
+      container
+      columnSpacing={{ md: 1 }}
+      rowSpacing={2}
+      paddingLeft={7}
+      marginTop={4}
+      marginBottom={4}
     >
       <Grid item xs={12} sm={6} md={4}>
         <Box>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://www.shutterstock.com/image-photo/image-butterfly-agama-lizard-leiolepis-600w-659854906.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
+          <BlogCard />
         </Box>
       </Grid>
 
       <Grid item xs={12} sm={6} md={4}>
         <Box>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://www.shutterstock.com/image-photo/image-butterfly-agama-lizard-leiolepis-600w-659854906.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
+          <BlogCard />
         </Box>
       </Grid>
 
       <Grid item xs={12} sm={6} md={4}>
         <Box>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://www.shutterstock.com/image-photo/image-butterfly-agama-lizard-leiolepis-600w-659854906.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
+          <BlogCard />
         </Box>
       </Grid>
 
       <Grid item xs={12} sm={6} md={4}>
         <Box>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://www.shutterstock.com/image-photo/image-butterfly-agama-lizard-leiolepis-600w-659854906.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
+          <BlogCard />
         </Box>
       </Grid>
 
       <Grid item xs={12} sm={6} md={4}>
         <Box>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://www.shutterstock.com/image-photo/image-butterfly-agama-lizard-leiolepis-600w-659854906.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
+          <BlogCard />
         </Box>
       </Grid>
     </Grid>
