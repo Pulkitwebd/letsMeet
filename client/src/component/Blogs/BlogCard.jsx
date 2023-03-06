7ed6da225c02c8d3a8507d825960968a67655636
Pulkitwebd <pulkitgupta714@gmail.com>
2023-03-06 17:35:34 +0530

    use props instead of destrucing

--- client/src/component/Blogs/BlogCard.jsx
+++ client/src/component/Blogs/BlogCard.jsx
@@ -5,33 +5,25 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import classes from "./Blogs.module.css";
 
-const BlogCard = ({
-  title,
-  authorName,
-  authorEmail,
-  lastEdited,
-  category,
-  creationDateAndTime,
-  imageUrl,
-  description,
-}) => {
+
+const BlogCard = (props) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <div className={classes.yogaimage}>
-        <img src={imageUrl} alt="" />
-        <div className={classes.category}>{category}</div>
+      <div className={classes.imageBox}>
+        <img src={props.imageUrl} alt="" />
+        <div className={classes.category}>{props.category}</div>
       </div>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {title}
+          {props.title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {description}
+          {props.description}
         </Typography>
       </CardContent>
       <CardActions>
-        <div size="small">{authorName} |</div>
-        <div size="small">{creationDateAndTime}</div>
+        <div size="small">{props.authorName} |</div>
+        <div size="small">{props.creationDateAndTime}</div>
       </CardActions>
       {/* <Button size="small">{category}</Button> */}
     </Card>
