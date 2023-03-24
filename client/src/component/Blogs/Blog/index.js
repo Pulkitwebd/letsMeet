ddf7bb275f9e4e87183090a1852ed1de349425ef
Pulkitwebd <pulkitgupta714@gmail.com>
2023-03-24 22:47:32 +0530

    edit css of blog individual page

--- client/src/component/Blogs/Blog/index.js
+++ client/src/component/Blogs/Blog/index.js
@@ -16,81 +16,78 @@ const Blog = () => {
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState(0);
   const { id } = useParams();
-  const Redheart = () => {
-    return (
-      <div>
-        <p title="Liked" className={classes.liked}>
-          <FcLike />
-        </p>
-        <span className="likeNum" title="View likes">{likes}</span>
-      </div>
-    );
-  };
 
   const handleLikeClick = () => {
-    setIsLiked(true);
-    setLikes(likes+1);
+    setIsLiked(!isLiked);
+    likes == 1 ? setLikes(0) : setLikes(1);
   };
 
   console.log(cards[id]);
   return (
     <div className={classes.container}>
       <h2 className={classes.cardTitle}>{cards[id - 1].title}</h2>
-      <div className={classes.BlogDetails}>
-        <h4 className={classes.aName}>By {cards[id - 1].authorName}</h4>
-        <div className={classes.timeDetails}>
-          <img className={classes.clockicon} src={clockicon} alt="" />
-          <h4 className={classes.creationDate}>
-            {cards[id - 1].creationDateAndTime}
-          </h4>
-        </div>
-        <div className={classes.readDetails}>
-          <img className={classes.readTimeicon} src={readicon} alt="" />
-          <h4 className={classes.readTime}>{cards[id - 1].timeToRead}</h4>
-        </div>
-      </div>
-      <div className={classes.imgContainer}>
-        <img src={cards[id - 1].imageUrl} alt="" />
-      </div>
-      <div className={classes.textContainer}>
-        <p className={classes.text}>{cards[id - 1].description}</p>
-        <div className={classes.bar}>
-          <div className={classes.iconContainer}>
-            <div>
-              {isLiked ? (
-                <Redheart />
-              ) : (
-                <p
-                  title="Like this blog"
-                  className={classes.like}
-                  onClick={handleLikeClick}
-                >
-                  <AiOutlineHeart /> <span className="likeNum" title="View likes">{likes}</span>
-                </p>
-              )}
-            </div>
 
-            <div>
-              <p title="Read comments" className={classes.comment}>
-                <BiCommentDetail />
-                <span>2</span>
-              </p>
-            </div>
-            <div>
-              <p title="Share" className={classes.share}>
-                <AiOutlineShareAlt />
-              </p>
-            </div>
-            <div>
-              <p title="Bookmark" className={classes.bookmark}>
-                <BsBookmark />
-              </p>
-            </div>
-            <div>
-              <p title="More options" className={classes.more}>
-                <AiOutlineMore />
-              </p>
-            </div>
+      <div className={classes.blogDetailsCover}>
+        <div className={classes.BlogDetails}>
+          <h4 className={classes.aName}>By {cards[id - 1].authorName}</h4>
+
+          <div className={classes.timeDetails}>
+            <img className={classes.clockicon} src={clockicon} alt="" />
+            <h4 className={classes.creationDate}>
+              {cards[id - 1].creationDateAndTime}
+            </h4>
+          </div>
+          <div className={classes.readDetails}>
+            <img className={classes.readTimeicon} src={readicon} alt="" />
+            <h4 className={classes.readTime}>{cards[id - 1].timeToRead}</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.img_textContainer}>
+        <div className={classes.imgContainer}>
+          <img src={cards[id - 1].imageUrl} alt="" />
+        </div>
+        <div className={classes.textContainer}>
+          <p className={classes.text}>{cards[id - 1].description}</p>
+        </div>
+      </div>
+      <div className={classes.bar}>
+        <div className={classes.iconContainer}>
+          <div>
+            <p
+              title="Like this blog"
+              className={classes.like}
+              onClick={handleLikeClick}
+            >
+              {isLiked ? <FcLike /> : <AiOutlineHeart />}
+              <span className="likeNum" title="View likes">
+                {likes}
+              </span>
+            </p>
+          </div>
+
+          <div>
+            <p title="Read comments" className={classes.comment}>
+              <BiCommentDetail />
+              <span>2</span>
+            </p>
+          </div>
+
+          <div>
+            <p title="Share" className={classes.share}>
+              <AiOutlineShareAlt />
+            </p>
+          </div>
+
+          <div>
+            <p title="Bookmark" className={classes.bookmark}>
+              <BsBookmark />
+            </p>
+          </div>
+
+          <div>
+            <AiOutlineMore title="More Options" />
           </div>
         </div>
       </div>
