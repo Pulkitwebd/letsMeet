34f46bee7e296cde0a5066ec5e29d0ed94e18d5d
Pulkitwebd <pulkitgupta714@gmail.com>
2023-05-23 16:11:28 +0530

    use image compression for event and user photo update and also modify event delete api

--- client/src/component/Dashboard/index.jsx
+++ client/src/component/Dashboard/index.jsx
@@ -1,29 +1,93 @@
-import React, { useState } from 'react';
-import classes from './index.module.css'; // Import the CSS module
+import React, { useState } from "react";
+import { SiBlogger } from "react-icons/si";
+import { BsCalendarEvent } from "react-icons/bs";
+import { RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+
+import classes from "./index.module.css";
+import BlogPart from "./Blog/index";
+import EventPart from "./Events/index";
 
 const Dashboard = () => {
-  const [isMinimized, setIsMinimized] = useState(false);
+  const [isMinimized, setIsMinimized] = useState(true);
+  const [currentPage, setCurrentPage] = useState(null);
+  const [showTotalUser, setShowTotalUser] = useState(true);
 
   const togglePanel = () => {
     setIsMinimized(!isMinimized);
   };
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleTotalUserCard = () => {
+    setShowTotalUser(!showTotalUser);
+  };
+
   return (
-    <div className={`${classes['sliding-panel']} ${isMinimized ? classes.minimized : ''}`}>
-      <button onClick={togglePanel} >{isMinimized ? 'Open' : 'Close'}</button>
-      {isMinimized ? (
-        <div className={classes['icons-container']}>
-          {/* Render your icons here */}
-          <div className={classes.icon}></div>
-          <div className={classes.icon}></div>
-          <div className={classes.icon}></div>
+    <div className={classes.dashboard}>
+      <div className={classes.header}>Header</div>
+      <div className={classes.sidebarAndMainDashboard}>
+        <div
+          className={isMinimized ? classes.sideBarClose : classes.sidebarOpen}
+        >
+          <div className={classes.arrowBox}>
+            <div className={classes.sidebarArrows} onClick={togglePanel}>
+              {isMinimized ? <RxDoubleArrowRight /> : <RxDoubleArrowLeft />}
+            </div>
+          </div>
+          {isMinimized && (
+            <div className={classes.sideBarIcons}>
+              <div onClick={() => handlePageChange("dashboard")}>
+                <MdOutlineDashboardCustomize />
+              </div>
+              <div onClick={() => handlePageChange("event")}>
+                <BsCalendarEvent />
+              </div>
+              <div onClick={() => handlePageChange("blog")}>
+                <SiBlogger />
+              </div>
+            </div>
+          )}
+          {!isMinimized && (
+            <div className={classes.sideBarIconsWithName}>
+              <div onClick={() => handlePageChange("dashboard")}>
+                <MdOutlineDashboardCustomize />
+                Dashboard
+              </div>
+              <div onClick={() => handlePageChange("event")}>
+                <BsCalendarEvent />
+                Event
+              </div>
+              <div onClick={() => handlePageChange("blog")}>
+                <SiBlogger /> Blog
+              </div>
+            </div>
+          )}
         </div>
-      ) : (
-        <div>
-          {/* Render the full content of the panel here */}
-          <p>Panel Content</p>
+        <div
+          className={
+            isMinimized ? classes.mainDashboardFull : classes.mainDashboardClose
+          }
+        >
+          {currentPage === "blog" && <BlogPart />}
+          {currentPage === "event" && <EventPart />}
+          {currentPage === "dashboard" && (
+            <div className={classes.smallCardsContainer}>
+              <div className={classes.smallCards} onClick={handleTotalUserCard}>
+                <p>{showTotalUser ? "Total User" : "User Added This Month"}</p>
+                <h4 className={classes.smallCardsNumber}>
+                  {showTotalUser ? "500" : "+54"}
+                </h4>
+              </div>
+              <div className={classes.smallCards}></div>
+              <div className={classes.smallCards}></div>
+              <div className={classes.smallCards}></div>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
