494c5f3838ff015b00a726eb2291ea60927ce7b9
Pulkit Gupta <pulkit@fundisspace.com>
2023-06-27 17:33:36 +0530

    Merge branch 'chandu' of https://github.com/Pulkitwebd/letsMeet

--- client/src/component/LandingPage/index.js
+++ client/src/component/LandingPage/index.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import classes from './index.module.css';
+import React from "react";
+import classes from "./index.module.css";
 import sideimage from "../Assets/Landing/80704-meeting.png";
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 
 const LandingPage = () => {
   return (
     <div>
-        <div className={classes.container}>
+      <div className={classes.container}>
         <Grid container>
-            <Grid item xs={12} md={6} lg={6}> 
-            <h1 className={classes.h1}>THE <span className={classes.header}>PLATFORM-</span> <br></br>WHERE YOU CAN INTERACT WITH THE WORLD ! </h1>
-              <button className={classes.btn}>Explore Events</button>
-            </Grid>
-            <Grid item xs={12} md={6} lg={6}>
-              <img src={sideimage} className={classes.img_preview} />
-             </Grid>
+          <Grid item xs={12} md={6} lg={6}>
+            <h1 className={classes.h1}>
+              THE <span className={classes.header}>PLATFORM-</span> <br></br>
+              WHERE YOU CAN INTERACT WITH THE WORLD !{" "}
+            </h1>
+            <button className={classes.btn}>Explore Events</button>
+          </Grid>
+          <Grid item xs={12} md={6} lg={6}>
+            <img alt="slider" src={sideimage} className={classes.img_preview} /> 
+          </Grid>
         </Grid>
-       
-            
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
