069cf768718bb0b48593e043e818b14a0c58f03c
Pulkit Gupta <pulkit@fundisspace.com>
2023-06-27 17:54:20 +0530

    add navigation in click explore event

--- client/src/component/LandingPage/index.js
+++ client/src/component/LandingPage/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import { useNavigate } from "react-router-dom";
+
 import classes from "./index.module.css";
 import sideimage from "../Assets/Landing/80704-meeting.png";
-import Grid from "@mui/material/Grid";
 
 const LandingPage = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className={classes.container}>
@@ -13,7 +18,7 @@ const LandingPage = () => {
               THE <span className={classes.header}>PLATFORM-</span> <br></br>
               WHERE YOU CAN INTERACT WITH THE WORLD !{" "}
             </h1>
-            <button className={classes.btn}>Explore Events</button>
+            <button className={classes.btn} onClick={() => navigate("/home")}>Explore Events</button>
           </Grid>
           <Grid item xs={12} md={6} lg={6}>
             <img alt="slider" src={sideimage} className={classes.img_preview} /> 
